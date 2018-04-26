82367346b31d6e9c0b6829d8cd0259d32e66ee44
pdesupinski <paul.b.desupinski.19@dartmouth.edu>
2018-04-26 14:56:11 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import './style.scss';
+import Complete from './components/Complete';
+import CarouselComp from './components/carousel';
+
 
 const {
   Header, Content, Footer, Sider,
@@ -16,14 +18,33 @@ ReactDOM.render(
       collapsedWidth="0"
       onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
     >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} />
+      <div className="logo" >
+        <Complete />
+      </div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} >
+        <Menu.Item key="1">
+          <Icon type="user" />
+          <span className="nav-text">nav 1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="video-camera" />
+          <span className="nav-text">nav 2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="upload" />
+          <span className="nav-text">nav 3</span>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Icon type="user" />
+          <span className="nav-text">nav 4</span>
+        </Menu.Item>
+      </Menu>
     </Sider>
     <Layout>
       <Header style={{ background: '#fff', padding: 0 }} />
       <Content style={{ margin: '24px 16px 0' }}>
         <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-          content
+          <CarouselComp />
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
